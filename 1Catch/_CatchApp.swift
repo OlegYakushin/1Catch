7627bbf6131504ec30b8e1571fe18711b6d62002
Oleg <yakushinoa@gmail.com>
2024-04-10 12:29:05 +0200

    Initial Commit

---
+++ 1Catch/_CatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  _CatchApp.swift
+//  1Catch
+//
+//  Created by Oleg Yakushin on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct _CatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
